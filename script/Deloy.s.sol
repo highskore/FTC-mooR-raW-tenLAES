c0a6ee265ee3686e64855696effaf25da006c4e5
Tanishk Goyal <goyaltanishk02@gmail.com>
2024-11-10 16:31:44 +0700

    deploy

---
+++ script/Deloy.s.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script} from "forge-std/Script.sol";
+
+contract MyScript is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        
+        vm.startBroadcast(deployerPrivateKey);
+
+
+        vm.stopBroadcast();
+    }
+}
